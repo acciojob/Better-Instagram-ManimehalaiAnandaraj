dbf7982d0ec613c9b060c704694c65680f23c4c3
ManimehalaiAnandaraj <manimehalaiannakodi@gmail.com>
2025-03-27 15:54:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
 //your code here
+const items=document.querySelectorAll('.image');
+let draggedItem=null;
+items.forEach(image =>{
+	image.addEventListener('dragstart',dragStart);
+	image.addEventListener('dragover',dragOver);
+	image.addEventListener('drop',dragDrop);
+});
+
+function dragStart(e) {
+	draggedItem=e.target;
+	e.dataTransfer.setData('text',e.target.id);
+	setTimeout(() =>{
+		e.target.style.visibility='hidden';
+	},0);
+}
+
+function dragOver(e) {
+	e.preventDefault();
+}
+
+function dragDrop(e) {
+	e.prevventDefault();
+	const targetItem=e.target;
+
+if (targetItem !== draggedItem && targetItem.classList.contains('image')) {
+      const draggedItemId = draggedItem.id;
+      const targetItemId = targetItem.id;
+
+      const draggedItemBg = getComputedStyle(draggedItem).backgroundImage;
+      const targetItemBg = getComputedStyle(targetItem).backgroundImage;
+      
+      draggedItem.style.backgroundImage = targetItemBg;
+      targetItem.style.backgroundImage = draggedItemBg;
+      
+      draggedItem.id = targetItemId;
+      targetItem.id = draggedItemId;
+  }
+  draggedItem.style.visibility = 'visible';
+  draggedItem = null;
+}
+
+
+
